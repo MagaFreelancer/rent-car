ed3a04e0a774ef01217fbb0167ced6a49c2010a5
YakhyaevGadji <gadji180105@gmail.com>
2024-11-27 01:20:29 +0300

    change: price style

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -6,7 +6,6 @@ import {
     getDrives,
     getDrivesActiveItems,
     getPriceFrom,
-    getPriceTo,
 } from '@/redux/slice/filters/filters-selectors.ts';
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/popover.tsx';
 import { RadioGroup, RadioGroupItem } from '@/shared/radio-group.tsx';
@@ -26,12 +25,9 @@ const Filters = () => {
     const brandsActiveItem = useAppSelector(getBrandActiveItem);
     const drivesActiveItems = useAppSelector(getDrivesActiveItems);
     const priceFrom = useAppSelector(getPriceFrom);
-    const priceTo = useAppSelector(getPriceTo);
     const brands = useAppSelector(getBrands);
     const drives = useAppSelector(getDrives);
     const dispatch = useAppDispatch();
-
-    console.log(priceFrom);
 
     const handleChangeRadio = (value: string) => {
         dispatch(setChangeBrands(value));
@@ -183,13 +179,13 @@ const Filters = () => {
                     <PopoverContent align="start" className="flex p-4 rounded-xl gap-4">
                         <Input
                             onKeyDown={event => handleSubmitPriceFrom(event)}
-                            className="h-12 bg-[#f2f4f8] text-[16px]"
+                            className="h-12 bg-[#f2f4f8] text-[16px] focus:bg-white"
                             type="number"
                             placeholder="500 ₽"
                         />
                         <Input
                             onKeyDown={event => handleSubmitPriceTo(event)}
-                            className="h-12 bg-[#f2f4f8] text-[16px]"
+                            className="h-12 bg-[#f2f4f8] text-[16px] focus:bg-white"
                             type="number"
                             placeholder="1000 ₽"
                         />
@@ -201,58 +197,3 @@ const Filters = () => {
 };
 
 export default Filters;
-
-// import { useAppDispatch, useAppSelector } from '@/redux/store.ts';
-// import { getFilters } from '@/redux/slice/filters/filters-selectors.ts';
-// import { Popover, PopoverContent, PopoverTrigger } from '@/shared/popover.tsx';
-// import { RadioGroup, RadioGroupItem } from '@/shared/radio-group.tsx';
-// import { Label } from '@/shared/label.tsx';
-// import { setChangeBrands } from '@/redux/slice/filters/filters-slice.ts';
-// import { clsx } from 'clsx';
-// import { ChevronUp, CircleX } from 'lucide-react';
-//
-// const Filters = () => {
-//     const { brands } = useAppSelector(getFilters);
-//     const dispatch = useAppDispatch();
-//
-//     const searchItem = brands.find((item) => item.status);
-//
-//     const handleChangeRadio = (value: string) => {
-//         dispatch(setChangeBrands(value));
-//     }
-//
-//     return (
-//         <div className="py-5">
-//             <div>
-//                 <Popover>
-//                     <div className={clsx('items-center gap-2 py-2 px-4 rounded-xl shadow bg-white inline-flex', searchItem?.value !== 'all' && '!bg-[#5394fd] text-white')}>
-//                         <PopoverTrigger>
-//                             Марка
-//                             {searchItem?.value !== 'all' && `: ${searchItem?.label}`}
-//                         </PopoverTrigger>
-//                         {searchItem?.value !== 'all' ? (
-//                             <CircleX onClick={() => handleChangeRadio('all')} className="z-50 w-4 z-55 h-4" />
-//                         ) : (
-//                             <ChevronUp className="w-5 h-5" />
-//                         )}
-//                     </div>
-//
-//                     <PopoverContent sideOffset={15} align="start" className="absolute left-[-15px] p-0">
-//                         <RadioGroup onValueChange={(item) => handleChangeRadio(item)} defaultValue={searchItem?.value}>
-//                             {brands?.map((item, index) => (
-//                                 <li className="list-none flex py-2 px-4 " key={index}>
-//                                     <Label className={clsx('flex gap-2 items-center w-full text-[16px]', searchItem?.value === 'all' && '')} htmlFor={item.value}>
-//                                         <RadioGroupItem className="h-5 w-5 text-blue-600 focus:ring-blue-500 border-gray-300" value={item.value} id={item.value} />
-//                                         {item.label}
-//                                     </Label>
-//                                 </li>
-//                             ))}
-//                         </RadioGroup >
-//                     </PopoverContent>
-//                 </Popover>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default Filters;
