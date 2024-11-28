ddee5aae1f4614200efca6d0145d8cc489d505af
Magomed Elmurzaev <ban0le4@gmail.com>
2024-11-28 04:59:38 +0300

    refactor file filters

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -37,16 +37,31 @@ const Filters = () => {
     const handleChangeCheckbox = (value: string) => {
         dispatch(setToggleDrives(value));
     };
+    const changePrice = () => {
+        let from = priceValue[0] === 0 ? undefined : priceValue[0];
+        let to = priceValue[1] === 0 ? undefined : priceValue[1];
 
-    const handleSubmitPrice = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
+        if (from !== undefined && to !== undefined) {
+            if (from > to) {
+                return false; //временное решение
+            }
             dispatch(
                 setChangePrice({
-                    from: priceValue[0] === 0 ? undefined : priceValue[0],
-                    to: priceValue[1] === 0 ? undefined : priceValue[1],
+                    from: from,
+                    to: to,
                 })
             );
         }
+    };
+    const handleSubmitPrice = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            changePrice();
+        }
+    };
+    const clearPrice = (key: 'from' | 'to') => {
+        let keyNum = key === 'from' ? 0 : 1;
+        priceValue[keyNum] = undefined;
+        changePrice();
     };
 
     // const handleSubmitPriceTo = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -160,51 +175,50 @@ const Filters = () => {
                         </PopoverContent>
                     </Popover>
                 ))}
+                {price.from === undefined && price.to !== undefined ? null : (
+                    <Popover>
+                        <PopoverTrigger
+                            className={clsx(
+                                'gap-1 shadow items-center py-2 px-4 flex rounded-xl bg-white',
+                                price.from && '!bg-[#5394fd] text-white'
+                            )}
+                        >
+                            {price.from ? `от ${price.from} ₽` : 'Цена, ₽'}
+                            {price.from !== undefined ? (
+                                <CircleX
+                                    onClick={() => clearPrice('from')}
+                                    className=" z-50 w-4 z-55 h-4"
+                                />
+                            ) : (
+                                <ChevronUp className="w-5 h-5" />
+                            )}
+                        </PopoverTrigger>
 
-                <Popover>
-                    <PopoverTrigger
-                        className={clsx(
-                            'gap-1 shadow items-center py-2 px-4 flex rounded-xl bg-white',
-                            price.from && '!bg-[#5394fd] text-white'
-                        )}
-                    >
-                        {price.from ? `от ${price.from} ₽` : 'Цена, ₽'}
-                        {price.from !== undefined ? (
-                            <CircleX
-                                onClick={e => {
-                                    // e.stopPropagation();
-                                    // handleChangeRadio('all');
+                        <PopoverContent align="start" className="flex p-4 rounded-xl gap-4">
+                            <Input
+                                onKeyDown={event => handleSubmitPrice(event)}
+                                className="h-12 bg-[#f2f4f8] text-[16px] focus:bg-white"
+                                type="number"
+                                placeholder="500 ₽"
+                                value={priceValue[0] || ''}
+                                onInput={e => {
+                                    setPriceValue([Number(e.currentTarget.value), priceValue[1]]);
                                 }}
-                                className=" z-50 w-4 z-55 h-4"
                             />
-                        ) : (
-                            <ChevronUp className="w-5 h-5" />
-                        )}
-                    </PopoverTrigger>
+                            <Input
+                                onKeyDown={event => handleSubmitPrice(event)}
+                                className="h-12 bg-[#f2f4f8] text-[16px] focus:bg-white"
+                                type="number"
+                                placeholder="1000 ₽"
+                                value={priceValue[1] || ''}
+                                onInput={e => {
+                                    setPriceValue([priceValue[0], Number(e.currentTarget.value)]);
+                                }}
+                            />
+                        </PopoverContent>
+                    </Popover>
+                )}
 
-                    <PopoverContent align="start" className="flex p-4 rounded-xl gap-4">
-                        <Input
-                            onKeyDown={event => handleSubmitPrice(event)}
-                            className="h-12 bg-[#f2f4f8] text-[16px] focus:bg-white"
-                            type="number"
-                            placeholder="500 ₽"
-                            value={priceValue[0] || ''}
-                            onInput={e => {
-                                setPriceValue([Number(e.currentTarget.value), priceValue[1]]);
-                            }}
-                        />
-                        <Input
-                            onKeyDown={event => handleSubmitPrice(event)}
-                            className="h-12 bg-[#f2f4f8] text-[16px] focus:bg-white"
-                            type="number"
-                            placeholder="1000 ₽"
-                            value={priceValue[1] || ''}
-                            onInput={e => {
-                                setPriceValue([priceValue[0], Number(e.currentTarget.value)]);
-                            }}
-                        />
-                    </PopoverContent>
-                </Popover>
                 {price.to !== undefined && (
                     <Popover>
                         <PopoverTrigger
@@ -216,10 +230,7 @@ const Filters = () => {
                             {`До ${price.to} ₽`}
                             {price.to !== undefined ? (
                                 <CircleX
-                                    onClick={e => {
-                                        // e.stopPropagation();
-                                        // handleChangeRadio('all');
-                                    }}
+                                    onClick={() => clearPrice('to')}
                                     className=" z-50 w-4 z-55 h-4"
                                 />
                             ) : (
