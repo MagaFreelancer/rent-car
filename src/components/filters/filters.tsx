b30fc0b8930fd1c4c7d4dd189da1082f8b657bd3
gadji <gadji180105@gmail.com>
2024-12-21 19:31:15 +0300

    change: filter price

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -16,7 +16,6 @@ import FilterPrice from '@/components/filters/components/filter-price.tsx';
 import Sort from '@/components/filters/components/sort/sort.tsx';
 import BrandsSort from '@/components/filters/components/brands-sort/brands-sort.tsx';
 import FilterDrives from '@/components/filters/components/filter-drives/filter-drives.tsx';
-import FiltersAll from '@/components/filters/components/filters-all/filters-all.tsx';
 
 const Filters = () => {
     const brandsActiveItem = useAppSelector(getBrandActiveItem);
@@ -44,7 +43,7 @@ const Filters = () => {
 
                 <FilterPrice />
 
-                <FiltersAll />
+                {/*<FiltersAll />*/}
             </div>
 
             <Sort
